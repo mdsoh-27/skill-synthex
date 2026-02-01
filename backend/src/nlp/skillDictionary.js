6d05686bfaf6817a534d1caf6dcaa4d59f7ac46c
Sohail Mohammed <sohail2705md@gmail.com>
2026-02-01 13:45:42 +0530

    error fix

--- backend/src/nlp/skillDictionary.js
+++ backend/src/nlp/skillDictionary.js
@@ -1,40 +1,28 @@
 module.exports = [
-  "python", "java", "javascript", "node", "express", "react", "sql", "mysql", "mongodb", "aws",
-  "machine learning", "data science", "nlp", "html", "css", "vue", "angular", "docker", "kubernetes",
-  "typescript", "nextjs", "git", "firebase", "sqlite", "postgresql", "c++", "c#", ".net", "php", "laravel"
-  , "selenium",
-  "beautifulsoup4",
-  "flask",
-  "langchain",
-  "faiss",
-  "huggingface",
-  "react.js",
-  "typescript",
-  "node.js",
-  "express",
-  "sql",
-  "mysql",
-  "mongodb",
-  "aws",
-  "machine learning",
-  "data science",
-  "nlp",
-  "html",
-  "css",
-  "vue",
-  "angular",
-  "docker",
-  "kubernetes",
-  "typescript",
-  "nextjs",
-  "git",
-  "firebase",
-  "sqlite",
-  "postgresql",
-  "c++",
-  "c#",
-  ".net",
-  "php",
-  "laravel",
+  // --- Languages ---
+  "python", "javascript", "typescript", "java", "c++", "c#", "go", "rust", "php", "ruby", "swift", "kotlin", "dart", "sql",
 
+  // --- Frontend Tech ---
+  "html", "css", "react", "react.js", "next.js", "nextjs", "vue", "vue.js", "angular", "svelte", "tailwind", "tailwind css", "bootstrap", "sass", "less", "redux", "zustand", "framer motion", "webpack", "vite",
+
+  // --- Backend Tech ---
+  "node", "node.js", "express", "express.js", "flask", "django", "fastapi", "spring boot", "laravel", "rails", "nestjs", "graphql", "apollo", "prisma", "sequelize", "typeorm", "mongoose", "socket.io", "web sockets",
+
+  // --- Mobile Tech ---
+  "react native", "flutter", "ionic", "xamarin",
+
+  // --- Databases ---
+  "mysql", "postgresql", "postgres", "mongodb", "redis", "firebase", "sqlite", "cassandra", "elasticsearch", "neo4j", "mariadb", "oracle",
+
+  // --- Cloud & DevOps ---
+  "aws", "azure", "gcp", "google cloud", "docker", "kubernetes", "k8s", "terraform", "ci/cd", "jenkins", "github actions", "gitlab ci", "nginx", "apache", "linux", "bash", "shell", "ansible", "prometheus", "grafana",
+
+  // --- AI / ML / NLP / LLMs ---
+  "machine learning", "deep learning", "nlp", "natural language processing", "data science", "pytorch", "tensorflow", "scikit-learn", "keras", "opencv", "numpy", "pandas", "matplotlib", "seaborn", "langchain", "llama-index", "openai", "huggingface", "pinecone", "chromadb", "faiss", "milvus", "vector databases", "prompt engineering",
+
+  // --- RAG & Extraction ---
+  "rag", "retrieval augmented generation", "extraction", "text processing", "optical character recognition", "ocr", "pdf parsing", "beautifulsoup4", "selenium", "puppeteer",
+
+  // --- Tools & Others ---
+  "git", "github", "gitlab", "bitbucket", "jira", "postman", "swagger", "auth0", "jwt", "oauth", "agile", "scrum", "microservices", "serverless", "distributed systems", "pwa", "spa"
 ];
